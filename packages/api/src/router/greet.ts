b36924dd2203a4e0a88afa7ff0d5cc6005cd430e
RollemIra <rollemira@users.noreply.github.com>
2023-10-20 00:47:11 -0400

    ever closer

--- packages/api/src/router/greet.ts
+++ packages/api/src/router/greet.ts
@@ -1,14 +1,11 @@
 import { z } from "zod";
 
-//import { schema } from "@acme/db";
-
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const greetRouter = createTRPCRouter({
   greeting: publicProcedure
     .input(z.object({ name: z.string() }))
     .query(async ({ input, ctx }) => {
-      //, ctx }) => {
       const leads = await ctx.db.query.leads.findMany();
       console.log("leads", leads);
       return { reply: `Greetings, ${input.name}` }; //. x-user?: ${ctx.user}.`;

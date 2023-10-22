a4ad5f5c6fa332b361207d4d4119bfefd235ff7b
RollemIra <rollemira@users.noreply.github.com>
2023-10-21 21:49:10 -0400

    passes root pnpm check

--- packages/api/src/router/greet.ts
+++ packages/api/src/router/greet.ts
@@ -5,7 +5,7 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 export const greetRouter = createTRPCRouter({
   greeting: publicProcedure
     .input(z.object({ name: z.string() }))
-    .query(async ({ input }) => {
+    .query(({ input }) => {
       return { reply: `Greetings, ${input.name}` }; //. x-user?: ${ctx.user}.`;
     }),
 });

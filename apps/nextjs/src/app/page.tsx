520d095e5bcbc8d3c4eb1ef06680479f142cbd4c
RollemIra <rollemira@users.noreply.github.com>
2023-10-22 22:55:58 -0400

    adds a nextjs website

---
+++ apps/nextjs/src/app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { name } from "@acme/calcs";
+
+import { api } from "../utils/api";
+
+export const runtime = "edge";
+
+export default function HomePage() {
+  const { data, error, isLoading } = api.greet.greeting.useQuery({
+    name: "nextjs",
+  });
+
+  if (isLoading) return <p className="text-center">Loading...</p>;
+  if (error) return <p className="text-center">Error: {error.message}</p>;
+
+  return (
+    <div className="flex w-full flex-col gap-4 text-center">
+      <h1 className="text-4xl">Next</h1>
+      <h2 className="text-xl">Loaded package</h2>
+      <p>{name}</p>
+      <h2 className="text-xl">tRPC Query</h2>
+      <pre>{data.reply}</pre>
+    </div>
+  );
+}
--- stacks/WebStack.ts
+++ stacks/WebStack.ts
@@ -1,11 +1,11 @@
-import { StackContext, StaticSite, use } from "sst/constructs";
+import { NextjsSite, StackContext, StaticSite, use } from "sst/constructs";
 import { ApiStack } from "./ApiStack";
 
 export function WebSiteStack({ stack }: StackContext) {
   const { api } = use(ApiStack);
 
   // Define our React app
-  const site = new StaticSite(stack, "WebSite", {
+  const viteSite = new StaticSite(stack, "WebSite", {
     path: "apps/web",
     buildCommand: "pnpm run build",
     buildOutput: "dist",
@@ -15,12 +15,23 @@ export function WebSiteStack({ stack }: StackContext) {
     },
   });
 
+  const nextSite = new NextjsSite(stack, "NextWebsite", {
+    path: "apps/nextjs",
+    // Pass in our environment variables
+    environment: {
+      NEXT_PUBLIC_API_URL: api.url,
+    },
+  });
+
+
   // Show the url in the output
   stack.addOutputs({
-    WebSiteUrl: site.url,
+    viteSiteUrl: viteSite.url,
+    nextSiteUrl: nextSite.url,
   });
 
   return {
-    site,
+    viteSite: viteSite,
+    nextSite: nextSite,
   };
 }

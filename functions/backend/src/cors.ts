ec15016e030e683f4743dfb26a4781db3426161b
RollemIra <rollemira@users.noreply.github.com>
2023-10-22 21:19:14 -0400

    match function config

--- functions/backend/src/cors.ts
+++ functions/backend/src/cors.ts
@@ -2,5 +2,4 @@ export const headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "*",
   "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
-  "Access-Control-Allow-Credentials": "true",
 };

504412450b8482cfca4a15da58db4113433fe2c3
RollemIra <rollemira@users.noreply.github.com>
2023-10-30 01:09:06 -0400

    fix for mobile tailwind config

--- tooling/tailwind/mobile.ts
+++ tooling/tailwind/mobile.ts
@@ -1,8 +1,9 @@
 import type { Config } from "tailwindcss";
 
-import baseConfig from "@acme/tailwind-config/mobile";
-
 export default {
   content: ["./src/**/*.{ts,tsx}"],
-  presets: [baseConfig],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
 } satisfies Config;

de0de42701424c79afcfc9c003dbd84cb5e24054
RollemIra <rollemira@users.noreply.github.com>
2023-10-31 00:32:58 -0400

    bug fixes for env, site deployed

--- stacks/WebStack.ts
+++ stacks/WebStack.ts
@@ -17,6 +17,9 @@ export function WebSiteStack({ stack }: StackContext) {
     path: "apps/gallery",
     // Pass in our environment variables
     environment: {
+      CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY!,
+      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+        process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!,
       NEXT_PUBLIC_API_URL: apiUrl,
     },
   });

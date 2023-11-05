35625daad63bafa3ded99f39e4da9ae212194110
RollemIra <rollemira@users.noreply.github.com>
2023-11-05 12:21:49 -0500

    adds fonts and themes back

--- apps/gallery/tailwind.config.ts
+++ apps/gallery/tailwind.config.ts
@@ -3,10 +3,9 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@acme/tailwind-config";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  //content: ["./src/**/*.{ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   presets: [baseConfig],
+  daisyui: {
+    themes: ["cmyk"],
+  },
 } satisfies Config;

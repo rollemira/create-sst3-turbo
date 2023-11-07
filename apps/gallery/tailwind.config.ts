2671b3461aaebbbb15f00f3a69d9ae4aee593122
RollemIra <rollemira@users.noreply.github.com>
2023-11-06 20:58:40 -0500

    bug fixes

--- apps/gallery/tailwind.config.ts
+++ apps/gallery/tailwind.config.ts
@@ -5,6 +5,17 @@ import baseConfig from "@acme/tailwind-config";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   presets: [baseConfig],
+  theme: {
+    fontFamily: {
+      "family-black": ["Geist-Black", "sans-serif"],
+      "family-thin": ["Geist-Thin", "sans-serif"],
+      "family-light": ["Geist-Light", "sans-serif"],
+      "family-normal": ["Geist-Regular", "sans-serif"],
+      "family-medium": ["Geist-Medium", "sans-serif"],
+      "family-semibold": ["Geist-SemiBold", "sans-serif"],
+      "family-bold": ["Geist-Bold", "sans-serif"],
+    },
+  },
   daisyui: {
     themes: ["cmyk"],
   },

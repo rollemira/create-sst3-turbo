527f685ffe28846df123da0f72c22b931352ecf0
RollemIra <rollemira@users.noreply.github.com>
2024-01-09 22:51:50 -0500

    housekeeping

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -14,7 +14,7 @@ export default {
   out: "./migrations",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: dbUrl,
+    uri: dbUrl,
   },
   tablesFilter: ["rollemtech_*"],
 } satisfies Config;
